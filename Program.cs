e23be58ed30c2d4064227b9c5edc950196ce82d9
Pelagein <68019687+Pelagein@users.noreply.github.com>
2023-02-24 15:16:04 +0300

    Добавил условие которое проверяет наличие новости у последнего студента.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@
                 Console.WriteLine("Введите количество студентов (2<=n<=100):");
                 countStudents = Convert.ToInt32(Console.ReadLine());
             } while (!(2 <= countStudents && countStudents <= 100));
+
             int[] studentsCountMessage = new int[countStudents + 1];
             for (int i = 1; i < studentsCountMessage.Length; i++)
             {
@@ -30,26 +31,25 @@
                 }
                 news[1] = 1; //Староста узнал о новости
 
-                for (int i = 1; (i < studentsCountMessage.Length && news[i] ==1 ); i++)
+                for (int i = 1; (i < studentsCountMessage.Length && news[i] == 1); i++)
                 {
                     for (int j = 0; j < studentsCountMessage[i]; j++)
                     {
-                        if ((i + 1 + j) < news.Length && news[i + 1 + j] == 0)
+                        if (news[countStudents] == 0)
                         {
-                            news[i + 1 + j] = 1; Console.WriteLine($"Студент № {i} отправил сообщение студенту №{i + 1 + j}");
+                            if ((i + 1 + j) < news.Length && news[i + 1 + j] == 0)
+                            {
+                                news[i + 1 + j] = 1; Console.WriteLine($"Студент № {i} отправил сообщение студенту №{i + 1 + j}");
+                            }
+                            else
+                            {
+                                studentsCountMessage[i] = studentsCountMessage[i] + 1;
+                            }
                         }
-                        else
-                        {
-                            studentsCountMessage[i] = studentsCountMessage[i] + 1;
-                        }
-
                     }
-
                 }
-
             }
         }
-
     }
 }
 
